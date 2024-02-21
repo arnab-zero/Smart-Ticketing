074d63b78abe02e9bdfd1353902184e3a54ae4e9
arnab-zero <darnabhere@gmail.com>
2024-02-22 03:28:52 +0600

    Completed JS functionality.

--- scripts/script.js
+++ scripts/script.js
@@ -8,45 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// phoneNumber.addEventListener('keyup', function handleEvent() {
-//     // toggle the next button
-//     if (phoneNumber.value !== null && seatCount !== 0) {
-//         toggleNext();
-//     }
-//     else if (phoneNumber.value === null) {
-//         toggleNext();
-//     }
-// })
-
-
-// change seat color and other counts related to seat if selected
-function handleSeatButtonClick(event) {
-    let seatButton = event.target;
-    const seatName = seatButton.innerText;
-    // booking a seat
-    if (seatButton.classList.contains('bg-gray-100') && seatCount < 4) {
-        seatButton.classList.remove('bg-gray-100', 'text-black');
-        seatButton.classList.add('bg-green-500', 'text-white');
-        seatCount = seatCount + 1;
-        addSeatToList(seatName);
-    }
-    // releasing a seat
-    else if (seatButton.classList.contains('bg-green-500')) {
-        seatButton.classList.remove('bg-green-500', 'text-white');
-        seatButton.classList.add('bg-gray-100', 'text-black');
-        seatCount = seatCount - 1;
-        removeSeatFromList(seatName);
-    }
-    // toggle the next button
-    if (phoneNumber.value !== null && seatCount !== 0) {
-        toggleNext();
-    }
-    bookedSeatCount.innerText = seatCount;
-    updateTotalTicketPrice();
-    updateGrandTotal(550 * seatCount);
-    updateRemainingSeatCount();
-}
-
 // add event to all seat buttons
 for (let i = 0; i < seatButtons.length; ++i) {
     seatButtons[i].addEventListener('click', handleSeatButtonClick);
@@ -58,11 +19,13 @@ couponApplyBtn.addEventListener('click', function () {
         updateGrandTotal(seatCount * 550 - seatCount * 550 * 0.15);
         //couponFeedback.innerText = 'Discount by coupon "NEW15": TK ' +  (seatCount*550*0.15).toString
         couponApplyBtn.disabled = true;
+        updateCouponDiv(seatCount * 550 * 0.15, 'NEW15');
     }
     else if (couponBox.value === 'Couple20') {
         updateGrandTotal(seatCount * 550 - seatCount * 550 * 0.2);
         //couponFeedback.innerText = 'Discount by coupon "Couple20": TK ' +  (seatCount*550*0.2).toString;
         couponApplyBtn.disabled = true;
+        updateCouponDiv(seatCount * 550 * 0.2, 'Couple20');
     }
     else {
         var couponFeedback = document.getElementById('coupon-feedback');
@@ -78,7 +41,7 @@ nextButton.addEventListener('click', function () {
     if (phoneNumber.value === '') {
         alert('Enter phone number to confirm tickets!')
     }
-    if(seatCount === 0){
+    if (seatCount === 0) {
         alert('Select a seat to continue booking.')
     }
     if (phoneNumber.value !== '' && seatCount !== 0) {
@@ -87,3 +50,6 @@ nextButton.addEventListener('click', function () {
         nextPage.classList.remove('hidden');
     }
 })
+
+
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -30,6 +30,35 @@ function handleSeatButtonClick(target) {
     target.classList.add("bg-green-500");
 }
 
+
+// change seat color and other counts related to seat if selected
+function handleSeatButtonClick(event) {
+    let seatButton = event.target;
+    const seatName = seatButton.innerText;
+    // booking a seat
+    if (seatButton.classList.contains('bg-gray-100') && seatCount < 4) {
+        seatButton.classList.remove('bg-gray-100', 'text-black');
+        seatButton.classList.add('bg-green-500', 'text-white');
+        seatCount = seatCount + 1;
+        addSeatToList(seatName);
+    }
+    // releasing a seat
+    else if (seatButton.classList.contains('bg-green-500')) {
+        seatButton.classList.remove('bg-green-500', 'text-white');
+        seatButton.classList.add('bg-gray-100', 'text-black');
+        seatCount = seatCount - 1;
+        removeSeatFromList(seatName);
+    }
+    // toggle the next button
+    if (phoneNumber.value !== null && seatCount !== 0) {
+        toggleNext();
+    }
+    bookedSeatCount.innerText = seatCount;
+    updateTotalTicketPrice();
+    updateGrandTotal(550 * seatCount);
+    updateRemainingSeatCount();
+}
+
 // print total ticket price
 function updateTotalTicketPrice() {
     const totalPrice = document.getElementById("total-price-section");
@@ -68,4 +97,37 @@ function removeSeatFromList(seatName) {
     const parentElement = removeElement.parentNode;
     parentElement.removeChild(removeElement);
 }
+
+// hide coupon-div child elements
+function updateCouponDiv(discount, couponName) {
+    const couponBox = document.getElementById('coupon-box');
+    couponBox.classList.add('hidden');
+    const couponApplyBtn = document.getElementById('coupon-apply-btn');
+    couponApplyBtn.classList.add('hidden');
+    // document.getElementById('coupon-box').classList.add('hidden');
+    // document.getElementById('coupon-apply-btn').classList.add('hidden');
+    addDiscountStatement(discount, couponName)
+}
+
+// function addDiscountStatement
+function addDiscountStatement(discount, couponName) {
+    const couponDiv = document.getElementById('coupon-div');
+    var newDiv = document.createElement('div');
+    newDiv.classList.add('grid', 'grid-cols-3', 'mb-5', 'text-md', 'text-green-500', 'font-base', 'w-full', 'mt-2');
+
+    // fix the content inside newDiv
+    var discountStatement = document.createElement('p');
+    discountStatement.classList.add("col-span-2")
+    discountStatement.innerText = `Discount [${couponName}]: `;
+    var discountPrice = document.createElement('p');
+    discountPrice.innerText = `- BDT ${discount}`;
+
+    // append children to newDiv
+    newDiv.appendChild(discountStatement);
+    newDiv.appendChild(discountPrice);
+
+    // append newDiv to couponDiv
+    couponDiv.appendChild(newDiv);
+}
+
 
